aac1b76345f536c6c3a6f24a72acf3795c8ae067
jim cosey <jimcosey@gmail.com>
2020-05-27 18:34:04 +0300

    effet button hover

--- js/menu-slide.js
+++ js/menu-slide.js
@@ -2,10 +2,14 @@ var span = 0;
 function toggle() {
   if (span == 0) {
     document.getElementById("arrow").style.transform = "rotate(-180deg)";
+    document.getElementById("arrow").style.color = "#f34335";
     document.getElementById("menu-slide").style.bottom = "-100%";
+    document.getElementById("container-slide_menu").style.backgroundColor =
+      "none";
     span = 1;
   } else {
     document.getElementById("arrow").style.transform = "rotate(0deg)";
+    document.getElementById("arrow").style.color = "#000";
     document.getElementById("menu-slide").style.bottom = "0";
     span = 0;
   }
