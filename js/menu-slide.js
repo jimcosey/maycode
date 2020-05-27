c7f4d51fdd3801a24c7c4d94cce791f0de88e1ca
jim cosey <jimcosey@gmail.com>
2020-05-27 16:45:30 +0300

    menu-modif-responsive-banner

--- js/menu-slide.js
+++ js/menu-slide.js
@@ -2,12 +2,10 @@ var span = 0;
 function toggle() {
   if (span == 0) {
     document.getElementById("arrow").style.transform = "rotate(-180deg)";
-    document.getElementById("container-slide_menu").style.bottom = "-100%";
     document.getElementById("menu-slide").style.bottom = "-100%";
     span = 1;
   } else {
     document.getElementById("arrow").style.transform = "rotate(0deg)";
-    document.getElementById("container-slide_menu").style.bottom = "0";
     document.getElementById("menu-slide").style.bottom = "0";
     span = 0;
   }
